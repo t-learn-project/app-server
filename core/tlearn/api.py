81d7c88a4d0a1c621007664d3e0e48d0faf3fac2
Mike0001-droid <majorov-2004@list.ru>
2022-11-24 00:32:13 +0500

    Create PUT request

--- core/tlearn/api.py
+++ core/tlearn/api.py
@@ -6,7 +6,6 @@ import datetime as datka
 
 router = Router()
 
-
 class Statistics(Schema):
     user_id: int
     card_id: int
@@ -31,8 +30,9 @@ def progress_post(request, payload: Statistics, otvet: int):
         card_id = payload.card_id,
         state = payload.state,
         time_created = payload.time_created,
-        penalty_step = payload.penalty_step
-        )
+        penalty_step = payload.penalty_step)
+    else:
+        return 'Вы знаете слово!'
 
 @router.get("/card/progress_get")
 def get_all_progress(request):
@@ -49,11 +49,75 @@ def get_all_progress(request):
             'now_time': data,
             'total': (data - stat.time_created).seconds,
         })
-    for i in range(len(CardUserProgr_list)):
-        if CardUserProgr_list[i]['state']!=0 and CardUserProgr_list[i]['total']>CardUserProgr_list[i]['state']:
-            return CardUserProgr_list[i] 
-            
-            
+    response = [] 
+    for i in CardUserProgr_list:
+        if i['state']!=0 and i['total'] > i['state']:
+            response.append(i)
+    return response
+
+@router.put("/card/progress_get")
+def update_carduserprogress(request, otvet: str):
+    CardUserProgr_list=[]
+    states = [0, 300, 3600, 86400, 604800, 2419200, 7257600, -1]
+    data = datka.datetime.now()
+    CardUserProgr_set = CardUserProgress.objects.all()
+    for stat in CardUserProgr_set:
+        CardUserProgr_list.append({
+            'user_id': stat.user_id,
+            'card_id': stat.card_id,
+            'state': stat.state,
+            'time_created': stat.time_created,
+            'penalty_step': stat.penalty_step,
+            'now_time': data,
+            'total': (data - stat.time_created).seconds,
+        })
+    dost = CardUserProgress.objects.all()
+    response = []
+    for i in CardUserProgr_list:
+        for j in range(len(states)-1):
+            if i['state']==states[j] and i['state']>0 and i['total'] > i['state'] and otvet == 'Да':
+                setattr(dost, 'state', states[j+1])
+                dost.save()
+                response.append(i)
+    return response
+                
+@router.get("/card/get_from_state")
+def get_all_cards_from_states(request, count: int):
+    CardUserProgr_list=[]
+    CardUserProgr_set = CardUserProgress.objects.all()
+    for stat in CardUserProgr_set:
+        CardUserProgr_list.append({
+            'user_id': stat.user_id,
+            'card_id': stat.card_id,
+            'state': stat.state,
+            'time_created': stat.time_created,
+            'penalty_step': stat.penalty_step,
+        })
+    CardList = []
+    CardSet = Card.objects.all()
+    for card in CardSet:
+        card_translations = card.translation.all()
+        CardList.append({
+            'id': card.id,
+            'collection': card.collection.name,
+            'word': card.word,
+            'transcription': card.transcription,
+            'translation': [i.word for i in card_translations],
+            'type': card.type
+        })
+    otvet = []
+    for i in CardUserProgr_list:
+        for j in CardList:
+            if i['card_id']==j['id']:
+                otvet.append(j)
+    return otvet
+""" @router.get("/card/count", response=CardsOut)
+def get_cards(request, count: int):
+    card_list = Card.objects.all()[:count]
+    return {
+        'count': len(card_list),
+        'cards': list(card_list)
+    }  """
 
 class Collection(Schema): 
     name: str
@@ -129,66 +193,13 @@ def get_cards(request, count: int):
 
 
 
-""" from time import gmtime, strftime
-hours = (int(strftime("%H", gmtime())))*3600
-minutes = (int(strftime("%M", gmtime())))*60
-seconds = int(strftime("%S", gmtime()))
-
-summa_seconds = hours+minutes+seconds
-
-
-showtime = strftime("%H:%M:%S", gmtime())
-
-print(showtime, summa_seconds) """
 
 
 
 
 
 
-
-
-
-
-
-""" class CardOut(Schema):
-    id: int
-    word: str
-    translation: str
-
-class CardsOut(Schema):
-    count: int
-    cards: List[CardOut]
-
-class CardIn(Schema):
-    collection_id: int
-    word: str
-    translation: str
-
-@router.get("/card/all", response=List[CardOut])
-def get_all_cards(request):
-    card_list = Card.objects.all()
-    return card_list
-
-@router.post("/card/add")
-def create_card(request, payload: CardIn):
-    new_card = Card.objects.create(
-        collection_id = payload.collection_id,
-        word = payload.word,
-        translation = payload.translation
-    )
-    return "Успешно!!!!!!!"
-    
-@router.post("/card/addlist")
-def create_cards(request, payload: List[CardIn]):
-    for card in payload:
-        new_card = Card.objects.create(
-            collection_id = card.collection_id,
-            word = card.word,
-            translation = card.translation
-        )
-    return "Успешно!!!!!!!"
-
+""" 
 @router.get("/card/count", response=CardsOut)
 def get_cards(request, count: int):
     card_list = Card.objects.all()[:count]
@@ -196,9 +207,40 @@ def get_cards(request, count: int):
         'count': len(card_list),
         'cards': list(card_list)
     } """
-
-
 """
 У карточки есть стейт, который определяет на какой станции находится слово
 По дефолту этот стейт равен 0, затем он обновляется, если пользователь нажал, что не знает слово 
 """
+
+
+""" 
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    department = models.ForeignKey(Department)
+    birthdate = models.DateField(null=True, blank=True)
+
+class EmployeeIn(Schema):
+    first_name: str
+    last_name: str
+    department_id: int = None
+    birthdate: date = None
+
+@api.put("/employees/{employee_id}")
+def update_employee(request, state: int, payload: Statistics):
+    employee = Card(Employee, id=state)
+    for attr, value in payload.dict().items():
+        setattr(employee, attr, value)
+    employee.save()
+    return {"success": True}
+
+
+
+
+
+
+
+
+ """
+
+
