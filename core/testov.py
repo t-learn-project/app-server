172e169b637050360c4d85e2e1453ba88036a719
Mike0001-droid <majorov-2004@list.ru>
2022-11-24 00:33:28 +0500

    Create PUT request

--- core/testov.py
+++
@@ -1,5 +0,0 @@
-carduserproglist = [{'time':1234, 'state':0}, {'time':1234, 'state':300}, {'time':1234, 'state':0}]
-states = [0, 300, 3600, 86400, 604800, 2419200, 7257600] 
-for i in carduserproglist:
-    print(i)
-    
